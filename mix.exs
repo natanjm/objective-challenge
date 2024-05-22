f06e596d2a89d68b3262dcad5ea3e05c1f60ae63
natanjm <natan.junges@hotmail.com>
2024-05-22 16:11:33 -0300

    chore: add excoveralls as dependency

--- mix.exs
+++ mix.exs
@@ -9,7 +9,14 @@ defmodule ObjectiveChallenge.MixProject do
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
@@ -42,6 +49,7 @@ defmodule ObjectiveChallenge.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
+      {:excoveralls, "~> 0.18", only: :test},
       {:plug_cowboy, "~> 2.5"}
     ]
   end

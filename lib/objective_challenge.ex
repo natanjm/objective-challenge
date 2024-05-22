905d43a4283f48cb645d4dfc0c22ba1d18938fd7
natanjm <natan.junges@hotmail.com>
2024-05-22 16:04:34 -0300

    feat: delegate functions to context domain

--- lib/objective_challenge.ex
+++ lib/objective_challenge.ex
@@ -6,4 +6,10 @@ defmodule ObjectiveChallenge do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+
+  alias ObjectiveChallenge.Bank
+
+  defdelegate create_account(params), to: Bank, as: :create_account
+  defdelegate get_account_by_number(account_number), to: Bank, as: :get_account_by_number
+  defdelegate create_transaction(params), to: Bank, as: :create_transaction
 end

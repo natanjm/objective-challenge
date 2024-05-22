b9e8ef1092a450a04d5191cbe61812bbda285c06
natanjm <natan.junges@hotmail.com>
2024-05-22 16:07:16 -0300

    refactor: implement error view

--- lib/objective_challenge_web/controllers/error_json.ex
+++ lib/objective_challenge_web/controllers/error_json.ex
@@ -1,15 +1,23 @@
 defmodule ObjectiveChallengeWeb.ErrorJSON do
-  # If you want to customize a particular status code,
-  # you may add your own clauses, such as:
-  #
-  # def render("500.json", _assigns) do
-  #   %{errors: %{detail: "Internal Server Error"}}
-  # end
+  import Ecto.Changeset, only: [traverse_errors: 2]
 
-  # By default, Phoenix returns the status message from
-  # the template name. For example, "404.json" becomes
-  # "Not Found".
-  def render(template, _assigns) do
-    %{errors: %{detail: Phoenix.Controller.status_message_from_template(template)}}
+  alias Ecto.Changeset
+
+  def error(%{result: %Changeset{} = changeset}) do
+    %{mesage: translate_errors(changeset)}
   end
+
+  def error(%{result: result}) do
+    %{message: result}
+  end
+
+  def translate_errors(changeset) do
+    traverse_errors(changeset, fn {msg, opts} ->
+      Enum.reduce(opts, msg, fn {key, value}, acc ->
+        String.replace(acc, "%{#{key}}", translate_value(value))
+      end)
+    end)
+  end
+
+  defp translate_value(value), do: to_string(value)
 end
